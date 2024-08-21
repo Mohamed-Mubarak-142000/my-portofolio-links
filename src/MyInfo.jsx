31d55a0fa02818c19cb6428246717fc5d0611a72
Mohamed Mubarak omar <mohamedmubarak142000m@gmail.com>
2024-08-21 23:57:03 +0300

    links

--- src/MyInfo.jsx
+++ src/MyInfo.jsx
@@ -3,7 +3,7 @@ const MyInfo = () => {
     <section className="my-info">
       <div>
         <img
-          src="../src/assets/WhatsApp Image 2024-07-11 at 17.32.10_ac8033e0.jpg"
+          src="./image/WhatsApp Image 2024-07-11 at 17.32.10_ac8033e0.jpg"
           alt="my_image"
         />
       </div>
